40b8a8268e9b860c7658402e8c33f0fbe478d06a
halealacan <alacanhale34@gmail.com>
2026-06-23 09:38:51 +0300

    özet

--- 22.06.2026/ilk-ders/src/pages/Home.jsx
+++ 22.06.2026/ilk-ders/src/pages/Home.jsx
@@ -2,10 +2,9 @@ export default function Home() {
   return (
     <div className="space-y-xl">
       <section className="mt-lg">
-        <h2 className="text-heading-lg">merhaba sınıf</h2>
-        <p className="text-body-md"> bugun ne öğrenmek isteriz</p>
+        <h2 className="text-heading-lg">Merhaba Sınıf</h2>
+        <p className="text-body-md">Bugün ne öğrenmmek isteriz?</p>
       </section>
-
       <section>
         <div className="search-bar-wrapper">
           <span className="material-symbols-outlined search-bar-icon">
@@ -14,12 +13,12 @@ export default function Home() {
           <input
             type="text"
             className="search-bar-input"
-            placeholder="Kurs eğitmen veya konu ara..."
+            placeholder="Kurs, eğitmen veya konu ara..."
           />
         </div>
       </section>
       <section className="category-chips-scroll no scrollbar">
-        <button className="category-chips-active">Tümü</button>
+        <button className="category-chip-active">Tümü</button>
         <button className="category-chip">Tasarım</button>
         <button className="category-chip">Yazılım</button>
         <button className="category-chip">Pazarlama</button>
@@ -35,7 +34,7 @@ export default function Home() {
               }}
             ></div>
             <div className="hero-text-container">
-              <span className="badge-tag">öne çıkan</span>
+              <span className="badge-tag">Öne Çıkan</span>
               <h3 className="text-display-lg mt-2">
                 UI/UX Tasarım Temelleri 2026
               </h3>
@@ -43,6 +42,45 @@ export default function Home() {
                 Profesyonel tasarımcı olmak için gereken her şey bu kursta
               </p>
               <button className="btn-primary mt-4">Şimdi Başla</button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="section-header">
+          <h2 className="text-headline-md">Popüler Kurslar</h2>
+          <div className="section-link">Tümünü Gör</div>
+        </div>
+        <div className="courses-scroll-container no-scrollbar">
+          <div className="course-card">
+            <div className="course-card-img-wrapper">
+              <img
+                src="https://lh3.googleusercontent.com/aida-public/AB6AXuAhQPwyGG5o1HbU4-h8MOObahKD78oEfMkzeLMvXSACqNXpNwa_OpT0APRj2Jr3r44OU3F6YjJSCZ_RMVXDOYGn7EoJDnE9T1SUZYRt4_qM0lCiofbNH7sfGjxGyUoPnrJ1ORx5z3V7CRCvIGW9z9_tsxqmcSBgKE6XDI3RslRpv7qVcj76JGDPXbAVUVcs-RavQ1DKAagNuntoUJsmoP23IPpJ8zyoNBI0nBVWOHo411oRWWaszwKtHuTG7FU36YLDQCLg2m-8Q9VW"
+                className="course-card-img"
+              ></img>
+              <span className="badge-bestseller-absolute">Çok Satan</span>
+            </div>
+            <div className="course-card-info">
+              <div className="course-card-title">İleri Seviye React.js</div>
+              <p className="course-card-author">Hale ALACAN</p>
+              <div className="course-card-rating-row">
+                <span className="text-label-md text-secondary">4.9</span>
+                <div className="course-card-rating-stars">
+                  <span className="material-symbols-outlined">star</span>
+                  <span className="material-symbols-outlined">star</span>
+                  <span className="material-symbols-outlined">star</span>
+                  <span className="material-symbols-outlined">star</span>
+                  <span className="material-symbols-outlined">star_half</span>
+                </div>
+                <span className="text-caption text-outline">(2.859)</span>
+              </div>
+              <div className="course-card-footer">
+                <span className="text-headline-md text-primary">259.99 TL</span>
+                <span className="material-symbols-outlined course-card-fav-btn">
+                  favorite
+                </span>
+              </div>
             </div>
           </div>
         </div>
